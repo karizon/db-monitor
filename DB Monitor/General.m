19c50346e005ea291742ed3402bd7e12296cd11b
Geoff Harrison <mandrake@mandrake.net>
2014-06-24 09:58:18 -0400

    Updating to the Apache 2 license

--- DB Monitor/General.m
+++ DB Monitor/General.m
@@ -3,7 +3,19 @@
 //  DB Monitor
 //
 //  Created by Geoff Harrison on 5/31/13.
-//  Copyright (c) 2013 mandrake. All rights reserved.
+//    Copyright 2014 Geoff Harrison
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import "General.h"
